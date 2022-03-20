6a8e55b7c1c01c11a138ce0cac8ea5e6b5e4173a
sukidhar <sukidhar@gmail.com>
2022-03-20 10:36:33 +0530

    docker set up complete

--- docker/phoenix/init.sh
+++ docker/phoenix/init.sh
@@ -4,8 +4,13 @@
 
 while ! nc -z neo4j 7687 ; do sleep 1 ; done
 
+echo 'export SENDGRID_API_KEY=${SENDGRID_KEY}' >> /root/.ashrc
 
+#clean up mac build cache and deps cache
+rm -rf deps
 mix setup
 mix assets.deploy
+
+echo $SENDGRID_API_KEY
 
 exec mix phx.server
